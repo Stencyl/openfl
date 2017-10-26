b56f742c438ddd9e20022a29092afdd44d860b73
Justin Espedal <justin.espedal@gmail.com>
2017-10-25 23:55:44 -0700

    Disable bitmap colorTransform

--- openfl/_internal/renderer/opengl/GLBitmap.hx
+++ openfl/_internal/renderer/opengl/GLBitmap.hx
@@ -41,7 +41,7 @@ class GLBitmap {
 			shader.data.uImage0.smoothing = renderSession.allowSmoothing && (bitmap.smoothing || renderSession.upscaled);
 			shader.data.uMatrix.value = renderer.getMatrix (bitmap.__renderTransform);
 			
-			var useColorTransform = !bitmap.__worldColorTransform.__isDefault ();
+			var useColorTransform = false;//!bitmap.__worldColorTransform.__isDefault ();
 			if (shader.data.uColorTransform.value == null) shader.data.uColorTransform.value = [];
 			shader.data.uColorTransform.value[0] = useColorTransform;
 			
@@ -53,7 +53,7 @@ class GLBitmap {
 			gl.vertexAttribPointer (shader.data.aTexCoord.index, 2, gl.FLOAT, false, 26 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
 			gl.vertexAttribPointer (shader.data.aAlpha.index, 1, gl.FLOAT, false, 26 * Float32Array.BYTES_PER_ELEMENT, 5 * Float32Array.BYTES_PER_ELEMENT);
 			
-			if (true || useColorTransform) {
+			/*if (true || useColorTransform) {
 				
 				gl.vertexAttribPointer (shader.data.aColorMultipliers.index, 4, gl.FLOAT, false, 26 * Float32Array.BYTES_PER_ELEMENT, 6 * Float32Array.BYTES_PER_ELEMENT);
 				gl.vertexAttribPointer (shader.data.aColorMultipliers.index + 1, 4, gl.FLOAT, false, 26 * Float32Array.BYTES_PER_ELEMENT, 10 * Float32Array.BYTES_PER_ELEMENT);
@@ -61,7 +61,7 @@ class GLBitmap {
 				gl.vertexAttribPointer (shader.data.aColorMultipliers.index + 3, 4, gl.FLOAT, false, 26 * Float32Array.BYTES_PER_ELEMENT, 18 * Float32Array.BYTES_PER_ELEMENT);
 				gl.vertexAttribPointer (shader.data.aColorOffsets.index, 4, gl.FLOAT, false, 26 * Float32Array.BYTES_PER_ELEMENT, 22 * Float32Array.BYTES_PER_ELEMENT);
 				
-			}
+			}*/
 			
 			gl.drawArrays (gl.TRIANGLE_STRIP, 0, 4);
 			
