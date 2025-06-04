2a113f75acbaaeef283269528ec2209dd037aa61
Josh Tynjala <joshtynjala@bowlerhat.dev>
2025-06-04 14:42:27 -0700

    OpenGLRenderer: fix scissor rect sizing when using openfl_dpi_aware define
    Followup to commits f626aa8d2db8ae656e0aa8c7e9f635093419fc3a and da0463a39863cf9caa86fe2d9716d7579251da5a

--- src/openfl/display/OpenGLRenderer.hx
+++ src/openfl/display/OpenGLRenderer.hx
@@ -771,6 +771,9 @@ class OpenGLRenderer extends DisplayObjectRenderer
 
 		if (__defaultRenderTarget == null)
 		{
+			#if openfl_dpi_aware
+			__scissorRectangle.setTo(__offsetX, __offsetY, __displayWidth, __displayHeight);
+			#else
 			if (__context3D.__backBufferWantsBestResolution)
 			{
 				__scissorRectangle.setTo(__offsetX / __pixelRatio, __offsetY / __pixelRatio, __displayWidth / __pixelRatio, __displayHeight / __pixelRatio);
@@ -779,6 +782,7 @@ class OpenGLRenderer extends DisplayObjectRenderer
 			{
 				__scissorRectangle.setTo(__offsetX, __offsetY, __displayWidth, __displayHeight);
 			}
+			#end
 			__context3D.setScissorRectangle(__scissorRectangle);
 
 			__upscaled = (__worldTransform.a != 1 || __worldTransform.d != 1);
@@ -838,6 +842,9 @@ class OpenGLRenderer extends DisplayObjectRenderer
 		}
 		else
 		{
+			#if openfl_dpi_aware
+			__scissorRectangle.setTo(__offsetX, __offsetY, __displayWidth, __displayHeight);
+			#else
 			if (__context3D.__backBufferWantsBestResolution)
 			{
 				__scissorRectangle.setTo(__offsetX / __pixelRatio, __offsetY / __pixelRatio, __displayWidth / __pixelRatio, __displayHeight / __pixelRatio);
@@ -846,6 +853,7 @@ class OpenGLRenderer extends DisplayObjectRenderer
 			{
 				__scissorRectangle.setTo(__offsetX, __offsetY, __displayWidth, __displayHeight);
 			}
+			#end
 			__context3D.setScissorRectangle(__scissorRectangle);
 			// __gl.viewport (__offsetX, __offsetY, __displayWidth, __displayHeight);
 
